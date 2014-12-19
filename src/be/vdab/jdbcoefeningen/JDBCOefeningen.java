ef129b8173ac3864ffd28f64b53289db56493c07
Andy.mertens <Andy.mertens@ONTCUR-0E18035F.vdabheverlee.intra>
2014-12-19 14:29:58 +0100

    testing completed

--- src/be/vdab/jdbcoefeningen/JDBCOefeningen.java
+++ src/be/vdab/jdbcoefeningen/JDBCOefeningen.java
@@ -5,15 +5,11 @@
  */
 package be.vdab.jdbcoefeningen;
 
-import java.sql.CallableStatement;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+
 
 /**
  *
